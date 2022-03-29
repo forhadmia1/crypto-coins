731ab1157fa81c17a3af446b06538f631cca002c
forhadmia1 <forhad8613@gmail.com>
2022-03-30 01:10:26 +0600

    header is done

---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coins = () => {
+    return (
+        <div>
+            this is coins page
+        </div>
+    );
+};
+
+export default Coins;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <nav>
-            
+        <nav className='flex justify-between items-center bg-cyan-700 px-16 py-3'>
+            <div className='text-2xl font-bold hidden md:block text-white'>
+                <Link to={'/'}>Crypto Coins</Link>
+            </div>
+            <div className='text-xl flex gap-6'>
+                <CustomLink to={'/'}>Home</CustomLink>
+                <CustomLink to={'/coins'}>Coins</CustomLink>
+                <CustomLink to={'/contact'}>Contact</CustomLink>
+                <CustomLink to={'/about'}>About</CustomLink>
+            </div>
         </nav>
     );
 };
